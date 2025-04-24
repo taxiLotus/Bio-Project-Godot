1dd8645a616ee3e97af157478e5fe57c5610c077
Axo Lotus <spacepenguin2323@gmail.com>
2025-04-23 19:23:21 -0700

    downstairs

--- bio-teaching-project/Game/Types/vocab_word.gd
+++ bio-teaching-project/Game/Types/vocab_word.gd
@@ -1,17 +1,26 @@
 extends Button
 
 var vocab_word: String
+var defOpen := false
+
+
 
 func set_word(word: String):
 	vocab_word = word
 	text = " " + vocab_word
 	
+var description = Label.new()
 
 	
 func _on_pressed() -> void:
-	var description = Label.new()
-	var node_index = get_index()
-	get_parent().add_child(description)
-	description.text = "    " + VocabList.vocab[vocab_word]
-	get_parent().move_child(description, node_index+1)
+	if defOpen == false:
+		description = Label.new()
+		var node_index = get_index()
+		get_parent().add_child(description)
+		description.text = "    " + VocabList.vocab[vocab_word]
+		get_parent().move_child(description, node_index+1)
+		defOpen = true
+	else:
+		description.queue_free()
+		defOpen = false
 	
