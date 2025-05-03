feb2e19d952e544dca973c9f4df8d8d8af9b86d2
Axo Lotus <spacepenguin2323@gmail.com>
2025-05-03 14:59:14 -0700

    upst

--- bio-teaching-project/Game/Scenes/screens/button.gd
+++ bio-teaching-project/Game/Scenes/screens/button.gd
@@ -1,22 +1,26 @@
-extends Button
+extends TextureButton
 
 var out = false
 var finalLoc = Vector2(861, 488)
 var velocity = Vector2.ZERO
 
-var icon_img = load("res://sprites/Caret-PNG-Image-470481549.png")
+var tex_norm = load("res://sprites/side_arrow.png")
 
 func _ready():
-	icon = icon_img
+	texture_normal = tex_norm
+	get_node("../ScrollContainer").vocab_pulled_out.connect(_on_vocab_pulled_out)
 
-func _on_button_pressed() -> void:
+func _on_vocab_pulled_out() -> void:
 	if out == false:
 		velocity.x = -5000
-		out = true
+		out = !out
 	else:
-		out = false
+		velocity.x = 5000
+		out = !out
 
 func _process(delta):
 	position += velocity * delta
 	if out == true and position.x < finalLoc.x:
+		velocity.x = 0
+	if out == false and position.x > 982.0:
 		velocity.x = 0
--- bio-teaching-project/Game/Scenes/screens/scroll_container.gd
+++ bio-teaching-project/Game/Scenes/screens/scroll_container.gd
@@ -4,13 +4,17 @@ var out = false
 var finalLoc = Vector2(956.0, 224)
 var velocity = Vector2.ZERO
 
+signal vocab_pulled_out
+
 func _on_button_pressed() -> void:
 	if out == false:
 		velocity.x = -5000
 		out = true
+		vocab_pulled_out.emit()
 	else:
 		velocity.x = 5000
 		out = false
+		vocab_pulled_out.emit()
 
 func _process(delta):
 	position += velocity * delta
