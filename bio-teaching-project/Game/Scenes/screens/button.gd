4f26f8d7d98b7dccc3c0dd413571309956dbc904
Axo Lotus <spacepenguin2323@gmail.com>
2025-05-02 20:48:31 -0700

    the FUYCKING vocab

--- bio-teaching-project/Game/Scenes/screens/button.gd
+++ bio-teaching-project/Game/Scenes/screens/button.gd
@@ -1,20 +1,22 @@
-extends ScrollContainer
+extends Button
 
 var out = false
-var finalLoc = Vector2(956.0, 224)
+var finalLoc = Vector2(861, 488)
 var velocity = Vector2.ZERO
+
+var icon_img = load("res://sprites/Caret-PNG-Image-470481549.png")
+
+func _ready():
+	icon = icon_img
 
 func _on_button_pressed() -> void:
 	if out == false:
 		velocity.x = -5000
 		out = true
 	else:
-		velocity.x = 5000
 		out = false
 
 func _process(delta):
 	position += velocity * delta
 	if out == true and position.x < finalLoc.x:
-		velocity.x = 0
-	if out == false and position.x > 1191.0:
 		velocity.x = 0
