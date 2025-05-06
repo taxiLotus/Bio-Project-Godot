497530fbb7890bd362367643cdcb98f317a1aff1
Axo Lotus <spacepenguin2323@gmail.com>
2025-05-05 20:31:46 -0700

    cooldown

---
+++ bio-teaching-project/Game/Types/button.gd
@@ -0,0 +1,36 @@
+extends TextureRect
+
+var shade_var = 0
+var shade_inc = 0
+var darkening = false
+
+func _on_atria_mouse_entered() -> void:
+	shade_var = 0.1
+	if(material.get_shader_parameter('difference') <= shade_var):
+		shade_inc = 0.1
+		darkening = true
+	else:
+		shade_inc = -0.1
+		darkening = false
+		
+		
+
+func _on_atria_mouse_exited() -> void:
+	shade_var = 0
+	if(material.get_shader_parameter('difference') <= shade_var):
+		shade_inc = 0.1
+		darkening = true
+	else:
+		shade_inc = -0.1
+		darkening = false
+	
+	
+func _process(delta: float) -> void:
+	material.set_shader_parameter('difference', material.get_shader_parameter('difference') + shade_inc)
+	
+	if darkening == true and material.get_shader_parameter('difference') >= shade_var:
+		shade_inc = 0
+		material.set_shader_parameter('difference', shade_var)
+	if darkening == false and material.get_shader_parameter('difference') <= shade_var:
+		shade_inc = 0
+		material.set_shader_parameter('difference', shade_var)
